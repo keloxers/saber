86b8d71ff47b1bf9a31d77953193fca231399193
Miguel Mendez <keloxers@gmail.com>
2018-08-09 14:26:08 -0300

    go

--- resources/views/categorias/impresion.blade.php
+++ resources/views/categorias/impresion.blade.php
@@ -7,9 +7,12 @@
 
   @if($categorias)
 
+  <table class="table">
+    <tbody>
+
 
       @foreach ($categorias as $categoria)
-
+      <tr>
 
       <h1>{{ $categoria->categoria}}</h1>
 
@@ -18,26 +21,26 @@
       @if($preguntas)
           @foreach ($preguntas as $pregunta)
 
+
+          <td>
           <p><b>*) {{ $pregunta->pregunta}}</b>
             @if($pregunta->url_foto<>"")
               <b>( CON FOTO SUBIDA)</b>
             @endif
           </p>
-
+          </td>
           <?php $respuestas = Respuesta::where('preguntas_id', $pregunta->id)->get(); ?>
 
 
 
               @if($respuestas)<p>
                   @foreach ($respuestas as $respuesta)
-
+                  <td>
                   {{ $respuesta->respuesta }}
                     @if($respuesta->correcta)
                       <b>( CORRECTA)</b>
                     @endif
-                    /
-
-
+                  </td>
                   @endforeach
                   </p>
               @endif
@@ -46,7 +49,11 @@
           @endforeach
       @endif
 
-
+      </tr>
       @endforeach
+
+    </tbody>
+  </table>
+
 
   @endif
