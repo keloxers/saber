03304e623d3c3a53e318789749c1ff32ddb9169b
Miguel Mendez <keloxers@gmail.com>
2022-08-11 13:27:21 -0300

    go

--- resources/views/rula/index.blade.php
+++ resources/views/rula/index.blade.php
@@ -17,9 +17,14 @@
               }
             }
     </script>
-    <h1>
-      <span id="countdown"></span>
-    <h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1>
+            <span id="countdown"></span>
+          </h1>
+      </div>
+    </div>
 
 <div class="container">
 
