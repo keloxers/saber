004787cecd4215526729f3917313b261f5d5247e
Miguel Mendez <keloxers@gmail.com>
2022-08-11 13:24:40 -0300

    go

--- resources/views/rula/index.blade.php
+++ resources/views/rula/index.blade.php
@@ -1,6 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
+
+<style>
+    body{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+      font-size: 16vw;
+    }
+  </style>
+  <script>
+            window.onload = updateClock;
+
+            var totalTime = 30;
+
+            function updateClock() {
+              document.getElementById('countdown').innerHTML = totalTime;
+              if(totalTime==0){
+                console.log('Final');
+              }else{
+                totalTime-=1;
+                setTimeout("updateClock()",1000);
+              }
+            }
+    </script>
+<span id="countdown"></span>
+
 <div class="container">
 
   <div class="row">
